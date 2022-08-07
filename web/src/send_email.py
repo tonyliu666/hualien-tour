8e97f2e25d60e9f2e2a2365afd71315768331997
TonyLiu <48583047+tonyliu666@users.noreply.github.com>
2022-08-07 20:52:55 +0800

    docker success

--- web/src/send_email.py
+++ web/src/send_email.py
@@ -13,8 +13,7 @@ class SendEmail(list):
         pack[0].config['MAIL_USE_TLS'] = False 
         pack[0].config['MAIL_USE_SSL'] = True
         mail = Mail(pack[0])
-            
-        topic = open('./templates/email_tour.html').read()
+        topic = open('./web/src/templates/email_tour.html').read()
         topic = topic.replace('{{first_tag}}',pack[1][0]).replace('{{second_tag}}',pack[1][1]).replace('{{third_tag}}',pack[1][2]).replace('{{fourth_tag}}',pack[1][3]).replace('{{fifth_tag}}',pack[1][4])
         subject = "為您精心挑選的旅遊指南"
         message = Message(subject,sender = "a0979277212@gmail.com",recipients=[pack[2]])
